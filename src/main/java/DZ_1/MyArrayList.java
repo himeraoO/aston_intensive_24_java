6faf1adcb63457cd47cd24d28dfaaf2400de6ef5
himeraoO <himer@bk.ru>
2023-03-09 13:40:05 +0300

    Dz1

--- src/main/java/DZ_1/MyArrayList.java
+++ src/main/java/DZ_1/MyArrayList.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class MyArrayList<E> implements List<E> {
 
     public MyArrayList(int cap) {
-        if (cap < 0){
+        if (cap < 0) {
             throw new RuntimeException("Размер не может быть меньше 0");
         }
         arr = new Object[cap];
@@ -32,7 +32,7 @@ public class MyArrayList<E> implements List<E> {
 
     @Override
     public boolean add(E e) {
-        if((arr.length - 1) == size){
+        if ((arr.length - 1) == size) {
             arrayResizing();
         }
         arr[size] = e;
@@ -40,19 +40,19 @@ public class MyArrayList<E> implements List<E> {
         return true;
     }
 
-    private void arrayResizing(){
+    private void arrayResizing() {
         int oldCap = arr.length;
-        if(oldCap > 0) {
+        if (oldCap > 0) {
             int newCap = (int) (oldCap * 1.5);
             arr = Arrays.copyOf(arr, newCap);
-        }else {
+        } else {
             arr = new Object[10];
         }
     }
 
-    public void trimToSize(){
-        if(size < arr.length){
-            if (size > 0){
+    public void trimToSize() {
+        if (size < arr.length) {
+            if (size > 0) {
                 arr = Arrays.copyOf(arr, size + 1);
             }
         }
@@ -62,15 +62,15 @@ public class MyArrayList<E> implements List<E> {
     @Override
     public boolean remove(Object o) {
         int i;
-        for(i = 0; i < size; i++) {
-            if (o.equals(arr[i])){
+        for (i = 0; i < size; i++) {
+            if (o.equals(arr[i])) {
                 break;
             }
         }
-        if(i == size) {
+        if (i == size) {
             return false;
         } else {
-            for(int j = i; j < size; j++){
+            for (int j = i; j < size; j++) {
                 arr[j] = arr[j + 1];
             }
             size--;
@@ -80,14 +80,14 @@ public class MyArrayList<E> implements List<E> {
 
     @Override
     public E remove(int index) {
-        if ((index >= 0) && (index < size)){
+        if ((index >= 0) && (index < size)) {
             E value = (E) arr[index];
-            for(int i = index; i < size; i++){
+            for (int i = index; i < size; i++) {
                 arr[i] = arr[i + 1];
             }
             size--;
             return value;
-        }else {
+        } else {
             throw new IndexOutOfBoundsException("Индекс " + index + " выходит за пределы размера " + size);
         }
     }
@@ -95,8 +95,8 @@ public class MyArrayList<E> implements List<E> {
     @Override
     public int indexOf(Object o) {
         int index = -1;
-        for(int i = 0; i < size; i++) {
-            if (o.equals(arr[i])){
+        for (int i = 0; i < size; i++) {
+            if (o.equals(arr[i])) {
                 index = i;
                 break;
             }
@@ -107,8 +107,8 @@ public class MyArrayList<E> implements List<E> {
     @Override
     public int lastIndexOf(Object o) {
         int index = -1;
-        for(int i = size - 1; i >= 0; i--) {
-            if (o.equals(arr[i])){
+        for (int i = size - 1; i >= 0; i--) {
+            if (o.equals(arr[i])) {
                 index = i;
                 break;
             }
@@ -118,8 +118,8 @@ public class MyArrayList<E> implements List<E> {
 
     @Override
     public boolean contains(Object o) {
-        for(int i = 0; i < size; i++) {
-            if (o.equals(arr[i])){
+        for (int i = 0; i < size; i++) {
+            if (o.equals(arr[i])) {
                 return true;
             }
         }
@@ -144,16 +144,16 @@ public class MyArrayList<E> implements List<E> {
 
         StringBuilder sb = new StringBuilder();
         sb.append("[");
-        for (Object o:arr) {
-            if (o == null){
+        for (Object o : arr) {
+            if (o == null) {
                 break;
             }
             sb.append(o);
             sb.append(", ");
         }
-        if (isEmpty()){
+        if (isEmpty()) {
             sb.append("]");
-        }else {
+        } else {
             sb.replace(sb.length() - 2, sb.length(), "]");
         }
         return sb.toString();
@@ -161,36 +161,78 @@ public class MyArrayList<E> implements List<E> {
 
     @Override
     public E get(int index) {
-        if ((index >= 0) && (index < (size))){
+        if ((index >= 0) && (index < (size))) {
             return (E) arr[index];
-        }else {
+        } else {
             throw new IndexOutOfBoundsException("Индекс " + index + " выходит за пределы размера " + size);
         }
     }
 
     @Override
     public E set(int index, E element) {
-        if ((index >= 0) && (index < (size))){
+        if ((index >= 0) && (index < (size))) {
             E oldValue = (E) arr[index];
             arr[index] = element;
             return oldValue;
-        }else {
+        } else {
             throw new IndexOutOfBoundsException("Индекс " + index + " выходит за пределы размера " + size);
         }
     }
 
     @Override
     public void add(int index, E element) {
-        if ((index >= 0) && (index < (size))){
-            if((arr.length - 1) == size){
+        if ((index >= 0) && (index < (size))) {
+            if ((arr.length - 1) == size) {
                 arrayResizing();
             }
             System.arraycopy(arr, index, arr, index + 1, size - index);
             arr[index] = element;
             size++;
-        }else {
+        } else {
             throw new IndexOutOfBoundsException("Индекс " + index + " выходит за пределы размера " + size);
         }
+    }
+
+    @Override
+    public void sort(Comparator<? super E> c) {
+        quickSort(0, size - 1, c);
+    }
+
+    public void quickSort(int from, int to, Comparator<? super E> c) {
+        if (to - from > 0) {
+            int partition = partition(from, to, c);
+            quickSort(from, partition - 1, c);
+            quickSort(partition, to, c);
+        }
+    }
+
+    private int partition(int from, int to, Comparator<? super E> c) {
+        E pivot = (E) arr[from + (to - from) / 2];
+        int li = from;
+        int ri = to;
+        while (li < ri) {
+
+            while (c.compare((E) arr[li], pivot) < 0) {
+                li++;
+            }
+
+            while ((ri > 0) && ((c.compare((E) arr[ri], pivot)) > 0)) {
+                ri--;
+            }
+
+            if (li <= ri) {
+                swap(li, ri);
+                li++;
+                ri--;
+            }
+        }
+        return li;
+    }
+
+    public void swap(int el1, int el2) {
+        E temp = (E) arr[el1];
+        arr[el1] = arr[el2];
+        arr[el2] = temp;
     }
 
     @Override
@@ -222,7 +264,7 @@ public class MyArrayList<E> implements List<E> {
 //        return null;
         throw new UnsupportedOperationException();
     }
-    
+
     @Override
     public boolean containsAll(Collection<?> c) {
 //        return false;
@@ -234,6 +276,7 @@ public class MyArrayList<E> implements List<E> {
 //        return false;
         throw new UnsupportedOperationException();
     }
+
 
     @Override
     public ListIterator<E> listIterator() {
